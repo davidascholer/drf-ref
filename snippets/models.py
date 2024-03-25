03feb7929ad43ebb1f4caaecb5fedf23ac905574
David Scholer <david@nimbio.com>
2024-03-24 22:28:11 -0700

    db nuke | add save to snippet model

--- snippets/models.py
+++ snippets/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+# added imports for formatting
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
+# added for formatting types of code options dialog
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+# added for formatting editor style of code options dialog
 STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 
@@ -14,6 +20,23 @@ class Snippet(models.Model):
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    # represents the user who created the code snippet
+    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+    # stores the highlighted HTML representation of the code
+    highlighted = models.TextField()
 
     class Meta:
         ordering = ['created']
+
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos,
+                                full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super().save(*args, **kwargs)
