54fb5ab50598cf6fe14a98a53e8d85171800d771
David Scholer <david@nimbio.com>
2024-03-26 16:42:41 -0700

    added pagination and viewsets

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -31,20 +31,25 @@ from django.contrib.auth.models import User
 #         return instance
 
 # DRM MODEL SERIALIZER WAY
-class SnippetSerializer(serializers.ModelSerializer):
-
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     # The source argument controls which attribute is used to populate a field, and can point at any attribute on the serialized instance.
     # We could have also used CharField(read_only=True)
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+# class SnippetSerializer(serializers.ModelSerializer):
+    # owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
 
 #Because 'snippets' is a reverse relationship on the User model, it will not be included by default when using the ModelSerializer class, so we needed to add an explicit field for it.
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+# class UserSerializer(serializers.ModelSerializer):
+#     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
+        fields = ['url', 'id', 'username', 'snippets']
--- snippets/urls.py
+++ snippets/urls.py
@@ -1,24 +1,82 @@
-from django.urls import path, include
-from snippets import views
-# added for formatting
-from rest_framework.urlpatterns import format_suffix_patterns
+# BASIC PATTERN
 
+# from django.urls import path, include
+# from snippets import views
+# # added for formatting
+# from rest_framework.urlpatterns import format_suffix_patterns
+
+# # API endpoints
 # urlpatterns = [
-#     path('snippets/', views.snippet_list),
-#     path('snippets/<int:pk>/', views.snippet_detail),
+#     path('', views.api_root),
+#     path('snippets/',
+#         views.SnippetList.as_view(),
+#         name='snippet-list'),
+#     path('snippets/<int:pk>/',
+#         views.SnippetDetail.as_view(),
+#         name='snippet-detail'),
+#     path('snippets/<int:pk>/highlight/',
+#         views.SnippetHighlight.as_view(),
+#         name='snippet-highlight'),
+#     path('users/',
+#         views.UserList.as_view(),
+#         name='user-list'),
+#     path('users/<int:pk>/',
+#         views.UserDetail.as_view(),
+#         name='user-detail'),
+#     path('api-auth/', include('rest_framework.urls')),
 # ]
 
-# refactor our snippets/urls.py slightly now that we're using class-based views.
+# # added for formatting
+# urlpatterns = format_suffix_patterns(urlpatterns)
 
+# BINDING VIEWSETS TO URLS EXPLICITLY (W BASIC PATTERN)
+
+# from rest_framework import renderers
+
+# from snippets.views import api_root, SnippetViewSet, UserViewSet
+
+# snippet_list = SnippetViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create'
+# })
+# snippet_detail = SnippetViewSet.as_view({
+#     'get': 'retrieve',
+#     'put': 'update',
+#     'patch': 'partial_update',
+#     'delete': 'destroy'
+# })
+# snippet_highlight = SnippetViewSet.as_view({
+#     'get': 'highlight'
+# }, renderer_classes=[renderers.StaticHTMLRenderer])
+# user_list = UserViewSet.as_view({
+#     'get': 'list'
+# })
+# user_detail = UserViewSet.as_view({
+#     'get': 'retrieve'
+# })
+
+# urlpatterns = format_suffix_patterns([
+#     path('', api_root),
+#     path('snippets/', snippet_list, name='snippet-list'),
+#     path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+#     path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+#     path('users/', user_list, name='user-list'),
+#     path('users/<int:pk>/', user_detail, name='user-detail')
+# ])
+
+# BINDING VIEWSETS WITH ROUTERS
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from snippets import views
+
+# Create a router and register our ViewSets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet, basename='snippet')
+router.register(r'users', views.UserViewSet, basename='user')
+
+# The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('', views.api_root),
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
-    path('api-auth/', include('rest_framework.urls')),
+    path('', include(router.urls)),
 ]
-
-# added for formatting
-urlpatterns = format_suffix_patterns(urlpatterns)
